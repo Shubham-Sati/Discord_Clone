ff9c7abf0028ce80105f4ce729238439ac19c4f7
Shubham Sati <68639424+Shubham-Sati@users.noreply.github.com>
2023-09-09 18:17:52 +0530

    Conversation setup done

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,10 +1,11 @@
 // this component is used both for one to one commuication and for the channel communication
 
-import { Hash } from "lucide-react";
+import { Hash, Mic, Video } from "lucide-react";
 
 import { MobileToggle } from "@/components/mobile-toggle";
 import { UserAvatar } from "@/components/user-avatar";
 // import { SocketIndicator } from "@/components/socket-indicator";
+// import { ChannelType } from "@prisma/client";
 
 // import { ChatVideoButton } from "./chat-video-button";
 
@@ -15,12 +16,20 @@ interface ChatHeaderProps {
   imageUrl?: string;
 }
 
+// const iconMap = {
+//   [ChannelType.TEXT]: Hash,
+//   [ChannelType.AUDIO]: Mic,
+//   [ChannelType.VIDEO]: Video,
+// };
+
 export const ChatHeader = ({
   serverId,
   name,
   type,
   imageUrl,
 }: ChatHeaderProps) => {
+  //   const Icon = type === "channel" ? iconMap[channel.type] : null;
+
   return (
     <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
       <MobileToggle serverId={serverId} />
